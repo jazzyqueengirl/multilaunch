cc1bf9047891b2f422ce5ee03d160eebb462744b
jazzyqueengirl <jazzyqueengirl@proton.me>
2025-03-01 11:09:47 +0000

    Fixed bug and updated dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
